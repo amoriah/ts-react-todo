ffa66d3d6a3f4158cdf81838903dd36702321b63
amoriah <yonoyko@bk.ru>
2024-02-06 20:41:20 +0300

    init

---
+++ jast-ts/test.js
@@ -0,0 +1,5 @@
+var firstName = "\u041E\u043B\u0435\u0441\u044F";
+var title = "\u0412\u0435\u0431-\u043F\u0440\u043E\u0433\u0440\u0430\u043C\u043C\u0438\u0441\u0442";
+var profile = "\u041C\u0435\u043D\u044F \u0437\u043E\u0432\u0443\u0442 ".concat(firstName, ". \n\u042F ").concat(title);
+var big = 9007199254740991n;
+console.log(profile, big);
---
+++ jast-ts/test.ts
@@ -0,0 +1,7 @@
+let firstName: string = `Олеся`;
+let title: string = `Веб-программист`;
+let profile: string = `Меня зовут ${firstName}. 
+Я ${title}`;
+let big: bigint = 9007199254740991n;
+
+console.log(profile, big);
